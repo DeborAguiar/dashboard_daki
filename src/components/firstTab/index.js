302dd17f8393a24b6b8e92dec31e4dfc7ff4fb03
DeborAguiar <50847540+DeborAguiar@users.noreply.github.com>
2023-08-07 16:18:30 -0300

    first chart

--- src/components/firstTab/index.js
+++ src/components/firstTab/index.js
@@ -6,27 +6,45 @@ import { Bar } from 'react-chartjs-2';
 
 function FirstTab() {
 
-    /* (IMPRESSIONS, CLICKS) , (GMV_USD, SALES_USD), (ORDERS, DAY_OF_WEEK) */
+    /* (IMPRESSIONS, CLICKS) , (GMV_USD, SALES_USD),  */
 
-    const [dataC, setDataC] = useState({
-        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
-        datasets: [
-            {
-                label: 'Vendas',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
-                borderWidth: 1,
-            },
-        ],
-    })
     const [data, setData] = useState([])
+    const [charts, setCharts] = useState({
+        /* (ORDERS, DAY_OF_WEEK) */
+        weekSales: {
+            labels: ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'],
+            datasets: [
+                {
+                    data: [],
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1,
+                },
+            ],
+        },
+        /* MALE_ORDERS,FEMALE_ORDERS */
+        genderComp: {
+            labels: ['Homens', 'Mulheres'],
+            datasets: [
+                {
+                    data: [],
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1,
+                },
+            ],
+        }
+    })
 
     useEffect(() => {
         fetchCSVData();
-    }, [data])
+    }, [])
 
-    async function fetchCSVData() {
+    useEffect(() => {
+        organizeData();
+    }, [data]);
+
+    const fetchCSVData = async () => {
         try {
             const response = await axios.get(file);
             const csvData = response.data;
@@ -38,8 +56,6 @@ function FirstTab() {
             });
         } catch (error) {
             console.error('Erro ao buscar o arquivo CSV:', error);
-        } finally {
-            organizeData()
         }
     };
 
@@ -55,27 +71,45 @@ function FirstTab() {
         };
         for (let reg of data) {
             if (days[reg.DAY_OF_WEEK]) {
-                days[reg.DAY_OF_WEEK]++
+                days[reg.DAY_OF_WEEK] += parseInt(reg.ORDERS) || 0
             }
         }
+
+        setCharts((prevCharts) => ({
+            ...prevCharts,
+            weekSales: {
+                ...prevCharts.weekSales,
+                datasets: [
+                    {
+                        ...prevCharts.weekSales.datasets[0],
+                        data: Object.values(days)
+                    },
+                ],
+            },
+        }));
     }
 
     return (
         <div className="carousel-item active h-100">
-            <div className="row row-cols-2 row-cols-lg-3 g-4 p-3">
+            <div className="row row-cols-1 row-cols-lg-3 g-4 p-3">
                 <div className="col">
                     <div className="p-3 charts">
-                        <Bar options={{
-                            title: {
-                                display: true,
-                                text: 'Average Rainfall per month',
-                                fontSize: 40
-                            },
-                            legend: {
-                                display: true,
-                                position: 'right'
-                            }
-                        }} data={dataC} />
+                        <h5>Vendas x Dia da Semana</h5>
+                        <Bar
+                            options={{
+                                scales: {
+                                    y: {
+                                        min: 1000,
+                                        max: 6000,
+                                    },
+                                },
+                                plugins:{
+                                    legend: {
+                                        display: false
+                                    },
+                                }
+                            }}
+                            data={charts.weekSales} />
                     </div>
                 </div>
                 <div className="col">
