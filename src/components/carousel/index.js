e5aa20ee3e126b207ab3bde4fae34edbbe65ecb5
DeborAguiar <50847540+DeborAguiar@users.noreply.github.com>
2023-08-07 18:44:42 -0300

    minor changes

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -5,10 +5,13 @@ import SecondTab from '../secondTab';
 function Carousel() {
     /* criacao do menu para mobile pode ser feito com o offcanva do bootstrap */
     return (
-        <div id="dashboards" className="carousel slide h-100 pt-3 pt-md-4">
-            <div className="carousel-inner h-100">
-                <FirstTab />
-                <SecondTab/>
+        <div>
+            <h3 className="pt-5">Análises</h3>
+            <div id="dashboards" className="carousel slide h-100 pt-3 pt-md-4">
+                <div className="carousel-inner h-100">
+                    <FirstTab />
+                    <SecondTab />
+                </div>
             </div>
         </div>
     )
