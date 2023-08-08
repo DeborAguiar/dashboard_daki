ea3314daa137cbbcfc63c9a647a3e66972853a60
DeborAguiar <50847540+DeborAguiar@users.noreply.github.com>
2023-08-08 18:12:40 -0300

    Fix carousel

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -5,7 +5,7 @@ import SecondTab from '../secondTab';
 function Carousel() {
     /* criacao do menu para mobile pode ser feito com o offcanva do bootstrap */
     return (
-        <div id="dashboards">
+        <div id="dash">
             <h3 className="pt-5">Análises</h3>
             <div id="dashboards" className="carousel slide pt-3 pt-md-4">
                 <div className="carousel-inner">
